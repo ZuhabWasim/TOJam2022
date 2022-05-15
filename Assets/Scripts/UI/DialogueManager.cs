b559282788f5d3dc06a9cc6777677dd7faea0a40
darrentran213 <85774691+darrentran213@users.noreply.github.com>
2022-05-14 21:29:05 -0400

    Dialog box test scene added

---
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class DialogueManager : MonoBehaviour
+{
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
+	
+	public Animator animator;
+	
+	private Queue<string> sentences;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+	
+	public void StartDialogue (Dialogue dialogue){
+		
+		animator.SetBool("isOpen",true);
+		nameText.text = dialogue.name;
+		sentences.Clear();
+		
+		foreach(string sentence in dialogue.sentences){
+			sentences.Enqueue(sentence);
+		}
+		
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence(){
+		if (sentences.Count == 0){
+			EndDialogue();
+			return;
+		}
+		
+		string sentence = sentences.Dequeue();
+		dialogueText.text = sentence;
+	}
+	
+	void EndDialogue(){
+		
+		animator.SetBool("isOpen",false);
+	}
+}
