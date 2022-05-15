0b15449545f3bb76bef022f7afe93067d81bcc4a
Valerie Roussel <59619956+ValerieRoussel@users.noreply.github.com>
2022-05-15 12:29:34 -0400

    Added camera limits

---
+++ Assets/Scripts/Camera/CameraLimits.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cinemachine;
+
+public class CameraLimits : CinemachineExtension
+{
+    public GameObject boundingRect;
+    public Camera c;
+
+    private float min_X, max_X, min_Y, max_Y;
+    private float cam_W, cam_H;
+
+    void Start() {
+        cam_H = c.orthographicSize;
+        cam_W = c.orthographicSize * c.aspect;
+
+        Bounds b = boundingRect.GetComponent<Renderer>().bounds;
+        min_X = b.min.x + cam_W;
+        min_Y = b.min.y + cam_H;
+        max_X = b.max.x - cam_W;
+        max_Y = b.max.y - cam_H;
+    }
+
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Finalize)
+        {
+            var pos = state.RawPosition;
+            pos.x = Mathf.Clamp(pos.x, min_X, max_X);
+            pos.y = Mathf.Clamp(pos.y, min_Y, max_Y);
+            state.RawPosition = pos;
+        }
+    }
+}
