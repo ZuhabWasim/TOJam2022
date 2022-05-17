9bbab7b24b99d9e552b4d05a7e6abe7f0638366e
Valerie Roussel <59619956+ValerieRoussel@users.noreply.github.com>
2022-05-17 00:01:27 -0400

    Level impl4 (#65)
    * Finalized levels and created main gameplay scene
    
    * Added proper room changing. Fixed snakes in desert.

--- Assets/Scripts/Camera/CameraLimits.cs
+++ Assets/Scripts/Camera/CameraLimits.cs
@@ -8,7 +8,9 @@ public class CameraLimits : CinemachineExtension
 {
     public GameObject boundingRect;
     public bool renderBg;
-    public Transform bg;
+    public Transform bgs;
+
+    public Transform[] bg_list;
 
     private float min_X, max_X, min_Y, max_Y;
     private Camera c;
@@ -19,11 +21,7 @@ public class CameraLimits : CinemachineExtension
         cam_H = c.orthographicSize;
         cam_W = c.orthographicSize * c.aspect;
 
-        Bounds b = boundingRect.GetComponent<Renderer>().bounds;
-        min_X = b.min.x + cam_W;
-        min_Y = b.min.y + cam_H;
-        max_X = b.max.x - cam_W;
-        max_Y = b.max.y - cam_H;
+        setBackground(boundingRect, 0);
     }
 
     protected override void PostPipelineStageCallback(
@@ -40,13 +38,40 @@ public class CameraLimits : CinemachineExtension
                 state.RawPosition = pos;
 
                 //background scrolling
-                if (bg != null && renderBg) {
+                if (bgs != null && renderBg) {
                     float xPerc, yPerc;
                     xPerc = (pos.x - min_X) / (max_X - min_X);
                     yPerc = (pos.y - min_Y) / (max_Y - min_Y);
-                    bg.transform.localPosition = new Vector3((float)(2.9f - (5.9 * xPerc)), (float)(5.95 - (11.9 * yPerc)), 10f);
+                    //hardcoded values since all backgrounds are the same size
+                    Vector3 p = new Vector3((float)(2.9f - (5.9 * xPerc)), (float)(5.95 - (11.9 * yPerc)), 10f);
+                    if (!float.IsNaN(p.x) && !float.IsNaN(p.y)) {
+                        bgs.transform.localPosition = p;
+                    }
                 }
             }
+        }
+    }
+
+    public GameObject getBackground() {
+        return boundingRect;
+    }
+
+    public void setBackground(GameObject bound, int bg_ind) {
+        boundingRect = bound;
+        Bounds b = boundingRect.GetComponent<Renderer>().bounds;
+        min_X = b.min.x + cam_W;
+        min_Y = b.min.y + cam_H;
+        max_X = b.max.x - cam_W;
+        max_Y = b.max.y - cam_H;
+        updateBackgroundImage(bg_ind - 1);
+    }
+
+    public void updateBackgroundImage(int bg_ind) {
+        for (int i=0; i<bg_list.Length; i++) {
+            bg_list[i].gameObject.SetActive(false);
+        }
+        if (bg_ind >= 0 && bg_ind < bg_list.Length) {
+            bg_list[bg_ind].gameObject.SetActive(true);
         }
     }
 }
