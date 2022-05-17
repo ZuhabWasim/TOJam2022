8d0cd6fdd2887902bb719b58df6294c262b4f83e
darrentran213 <85774691+darrentran213@users.noreply.github.com>
2022-05-17 08:12:26 -0400

    Menu goes to main gameplay scene

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -29,7 +29,7 @@ public class UIManager : MonoBehaviour
 
 		animator.SetTrigger("TriggerTransition");
 		
-		SceneManager.LoadScene("TestSceneLarge");
+		SceneManager.LoadScene("MainGameplayScene");
 	}
 	
 	public void CloseMenuSettings(){
